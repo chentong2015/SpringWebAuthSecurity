582b5da06c553ade5e835010b902bfe06970ef22
tchen <chen2015tong@gmail.com>
2024-11-25 15:17:15 +0100

    update auth login session web pages

--- auth-web-login-session/src/main/java/main/backend/controller/UserController.java
+++ auth-web-login-session/src/main/java/main/backend/controller/UserController.java
@@ -1,12 +1,12 @@
 package main.backend.controller;
 
+import main.backend.model.entity.UserEntity;
+import main.backend.service.UserService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +15,11 @@ import java.util.Map;
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
-    // Reset Password 重置当前用户的密码
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     // Update Password
     // @RequestMapping(value = "/changePassword", method = RequestMethod.POST)
@@ -33,6 +37,18 @@ public class UserController {
     // }
 
 
-    // Show me 显示当前的用户
-    // Find my user name
+    // 从Context中获取认证过的UserDetails
+    // Make sure user has role "ROLE_USER" to access this endpoint.
+    @GetMapping("/whoami")
+    @PreAuthorize("hasRole('USER')")
+    public UserEntity user() {
+        return (UserEntity) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+    }
+
+    // findUserByUsername()会确保用户具体指定的Role
+    @GetMapping("/showme/{username}")
+    public UserEntity showMe(@PathVariable String username) {
+        return this.userService.findUserByUsername(username);
+    }
 }
--- auth-web-login-session/src/main/java/main/backend/session/SessionController.java
+++ auth-web-login-session/src/main/java/main/backend/session/SessionController.java
@@ -1,20 +1,19 @@
 package main.backend.session;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jwt.JwtTokenProvider;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class SessionController {
 
-    @RequestMapping(value = "/refresh", method = RequestMethod.GET)
+    @GetMapping("/session/refresh")
     public ResponseEntity<?> refreshAuthenticationToken(HttpServletRequest request, HttpServletResponse response) {
         String authToken = CookieHandler.getTokenFromAuthHeader(request);
         if (authToken == null) {
